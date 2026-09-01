00fe1bd4971108c7fd00bc1e0024fe670b8efd72
Malorie Cooper <132147998+maloriedelilah@users.noreply.github.com>
2026-09-01 11:09:55 -0600

    Add lead provider interface

---
+++ src/lib/leads/types.ts
@@ -0,0 +1,10 @@
+export interface Lead {
+  email: string;
+  name?: string;
+  groups?: string[];
+}
+
+export interface LeadAdapter {
+  name: string;
+  subscribe(lead: Lead): Promise<{ ok: true }>;
+}
